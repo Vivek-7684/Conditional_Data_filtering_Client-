362290b8f56a17dab3d80eceef97715e7bbdc05f
Vivek-7684 <vivek.infosware@gmail.com>
2025-10-31 15:29:51 +0530

    add params in url and set ui

---
+++ src/Form.jsx
@@ -0,0 +1,68 @@
+import TextField from '@mui/material/TextField';
+import MenuItem from '@mui/material/MenuItem';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import Axios from 'axios';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import api from './api/api.js';
+import Box from '@mui/material/Box';
+import { useNavigate } from 'react-router-dom';
+
+export default function Form() {
+
+    const Navigate = useNavigate();
+
+    const [filter, setFilter] = useState({
+        "name": "",
+        "maxPrice": "",
+        "minPrice": "",
+        "category": ""
+    });
+
+    const handleChange = (e) => {
+        console.log(e.target.name);
+        console.log(e.target.value);
+        setFilter({ ...filter, [e.target.name]: e.target.value });
+    };
+
+    const handleSearch = async () => {
+
+        let params = [];
+
+        if (filter.name) params.push(`name=${filter.name}`);
+        if (filter.maxPrice) params.push(`maxPrice=${filter.maxPrice}`);
+        if (filter.minPrice) params.push(`minPrice=${filter.minPrice}`);
+        if (filter.category) params.push(`category=${filter.category}`);
+
+        const filterProduct = `/Product?${params.join('&')}`;
+
+        Navigate(filterProduct);
+
+        // await api.get(filterProduct)
+        //     .then((response) => { setProduct(response.data); console.log(response.data); })
+        //     .catch((err) => err.message)
+    }
+    console.log(filter);
+
+
+
+    return (
+        <Box sx={{ width: '50vw', p: 3, display: 'flex', flexDirection: 'row', gap: '1rem', justifyItems: 'center', alignContent: 'center' }}>
+            <form>
+                <Typography variant='h5'>Select Filter</Typography>
+                <TextField label="name" name="name" margin="normal" onChange={(e) => handleChange(e)} variant='outlined' fullWidth />
+                <TextField type='number' name="maxPrice" label="MaxPrice" margin="normal" onChange={(e) => handleChange(e)} variant='outlined' fullWidth />
+                <TextField type='number' name="minPrice" label="MinPrice" margin="normal" onChange={(e) => handleChange(e)} variant='outlined' fullWidth />
+                <TextField select label="category" name="category" value={filter.category} margin="normal" variant='outlined' onChange={(e) => handleChange(e)} fullWidth >
+                    <MenuItem value="Electronics">Electronics</MenuItem>
+                    <MenuItem value="Cloth">Cloth</MenuItem>
+                    <MenuItem value="Accessories">Accessories</MenuItem>
+                    <MenuItem value="Sports">Sports</MenuItem>
+                </TextField>
+
+                <Button variant='contained' fullWidth sx={{ p: 1 }} onClick={() => handleSearch()}>Get Product</Button>
+            </form>
+        </Box>
+    )
+}
---
+++ src/Product.jsx
@@ -0,0 +1,68 @@
+import Table from '@mui/material/Table';
+import TableContainer from '@mui/material/TableContainer';
+import TableBody from '@mui/material/TableBody';
+import TableRow from '@mui/material/TableRow';
+import TableHead from '@mui/material/TableHead';
+import TableCell from '@mui/material/TableCell';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import Axios from 'axios';
+import api from './api/api.js';
+import { TableFooter } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+
+
+
+export const Product = () => {
+
+    const [product, setProduct] = useState([]);
+
+    const navigate = useNavigate();
+
+    const location = useLocation();
+
+    const getAllProducts = () => {
+        api.get(`/Product${location.search}`)
+            .then((response) => { setProduct(response.data); console.log(response.data); })
+            .catch((err) => err.message)
+    }
+
+    useEffect(() => getAllProducts(), []);
+
+    return (
+        <Stack gap='2rem' sx={{ width: "50vw", p: 5 }}>
+            <Button variant='contained' sx={{ p: 1, width: '30%' }} onClick={() => { navigate('/'); }}>Back</Button>
+            <TableContainer sx={{ width: "70vw", height: "70vh" }}>
+                <Typography variant='h5'>My Products</Typography>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell><strong>Name</strong></TableCell>
+                            <TableCell><strong>MaxPrice</strong></TableCell>
+                            <TableCell><strong>MinPrice</strong></TableCell>
+                            <TableCell><strong>Catgory</strong></TableCell>
+                        </TableRow>
+                    </TableHead>
+
+                    <TableBody>
+                        {product.map((data) => {
+                            return (
+                                <TableRow key={data.name}>
+                                    <TableCell>{data.name}</TableCell>
+                                    <TableCell>{data.maxPrice}</TableCell>
+                                    <TableCell>{data.minPrice}</TableCell>
+                                    <TableCell>{data.category}</TableCell>
+                                </TableRow>
+                            )
+                        })}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+            <Button variant='contained' sx={{ p: 1, width: '100%' }} onClick={() => { getAllProducts(); }}>Get All Products</Button>
+        </Stack>
+    )
+}
